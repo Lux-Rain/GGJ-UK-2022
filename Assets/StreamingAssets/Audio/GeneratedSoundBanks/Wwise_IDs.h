29806fc1e286e938aa9823e8e7c35e65bcd93cbf
therankofjedimaster <98640726+therankofjedimaster@users.noreply.github.com>
2022-01-29 22:51:50 +0200

    Added new Music and UI SFX

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -19,13 +19,32 @@ namespace AK
         static const AkUniqueID CAMERA_ZOOM_OUT = 2043534011U;
         static const AkUniqueID FOOTSTEPS = 2385628198U;
         static const AkUniqueID JUMP_TRAMPOLINE = 2012276773U;
-        static const AkUniqueID MUSIC_GAMEPLAY = 620878633U;
+        static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID PHOTO_QUEST = 2364952868U;
         static const AkUniqueID PHOTO_RANDOM = 4068392799U;
         static const AkUniqueID SHOVEL_DIGGING = 2053301760U;
         static const AkUniqueID SHOVEL_TAKE = 1059993718U;
         static const AkUniqueID TREES = 3882419280U;
+        static const AkUniqueID UI_HOVER = 2118900976U;
+        static const AkUniqueID UI_PLAY = 2382194672U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC
+        {
+            static const AkUniqueID GROUP = 3991942870U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GAMEPLAY = 89505537U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TIME_CAPSULE = 3711779106U;
+            } // namespace STATE
+        } // namespace MUSIC
+
+    } // namespace STATES
 
     namespace SWITCHES
     {
@@ -52,7 +71,10 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENCE = 85412153U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID PHOTO = 3205522195U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
